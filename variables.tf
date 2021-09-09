7e87488e5bd3bc8280d607d832acedb78c9d0a21
PSSahana <75433106+PSSahana@users.noreply.github.com>
2021-09-09 16:39:06 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 
 variable "key_name" {
   description = " SSH keys to connect to ec2 instance"
-  default     =  "jenkins.pem"
+  default     =  "jenkins"
 }
 
 variable "instance_type" {
